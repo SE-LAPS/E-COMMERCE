c0289dae95e49da4e0e9545f73b33b94aa6f91df
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2025-01-18 08:26:11 +0530

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,71 +1,66 @@
-import { useSelector } from 'react-redux';
-import {
-  Navigate,
-  Route, RouterProvider, createBrowserRouter, createRoutesFromElements
-} from "react-router-dom";
-import { selectIsAuthChecked, selectLoggedInUser } from './features/auth/AuthSlice';
-import { Logout } from './features/auth/components/Logout';
-import { Protected } from './features/auth/components/Protected';
-import { useAuthCheck } from "./hooks/useAuth/useAuthCheck";
-import { useFetchLoggedInUserDetails } from "./hooks/useAuth/useFetchLoggedInUserDetails";
-import { AddProductPage, AdminOrdersPage, CartPage, CheckoutPage, ForgotPasswordPage, HomePage, LoginPage, OrderSuccessPage, OtpVerificationPage, ProductDetailsPage, ProductUpdatePage, ResetPasswordPage, SignupPage, UserOrdersPage, UserProfilePage, WishlistPage } from './pages';
-import { AdminDashboardPage } from './pages/AdminDashboardPage';
-import { NotFoundPage } from './pages/NotFoundPage';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import HomePage from './Pages/Home/HomePage';
+import Login from './Auth/Login/Login';
+import Register from './Auth/Register/Register';
+import Cart from './Pages/Cart/Cart';
+import ProductDetail from './Pages/Detail/ProductDetail';
+import SingleCategory from './SingleCategory/SingleCategory';
+import MobileNavigation from './Navigation/MobileNavigation';
+import DesktopNavigation from './Navigation/DesktopNavigation';
+import Wishlist from './Pages/WhisList/Wishlist';
+import PaymentSuccess from './Pages/Payment/PaymentSuccess';
+import { Flip, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import CheckoutForm from './Components/Checkout/CheckoutForm';
+import UpdateDetails from './Pages/Update_User/UpdateDetails';
+import ForgotPasswordForm from './Auth/ForgotPassword/ForgotPasswordForm';
+import AddNewPassword from './Auth/ForgotPassword/AddNewPassword';
+import AdminLogin from './Admin/Auth/Login/AdminLogin';
+import AdminRegister from './Admin/Auth/Register/AdminRegister';
+import AdminHomePage from './Admin/Pages/AdminHomePage';
+import SingleUserPage from './Admin/Pages/SingleUserPage';
+import SingleProduct from './Admin/Pages/SingleProduct';
+
+
+
 
 
 function App() {
+  return (
+    <>
+      <ToastContainer toastClassName='toastContainerBox' transition={Flip} position='top-center' />
+      <Router>
+        <DesktopNavigation />
+        <div className='margin'>
+          <Routes>
+            {/*User Routes  */}
+            <Route path='/' index element={<HomePage />} />
+            <Route path="/login" element={< Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/Detail/type/:cat/:id' element={<ProductDetail />} />
+            <Route path='product/type/:cat' element={<SingleCategory />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/wishlist' element={<Wishlist />} />
+            <Route path='/checkout' element={<CheckoutForm />} />
+            <Route path='/update' element={<UpdateDetails />} />
+            <Route path='/paymentsuccess' element={<PaymentSuccess />} />
+            <Route path='/forgotpassword' element={<ForgotPasswordForm />} />
+            <Route path='/user/reset/:id/:token' element={<AddNewPassword />} />
 
-  const isAuthChecked=useSelector(selectIsAuthChecked)
-  const loggedInUser=useSelector(selectLoggedInUser)
+            {/* Admin Routes */}
+            <Route path="/admin/login" element={< AdminLogin />} />
+            <Route path='/admin/register' element={<AdminRegister />} />
+            <Route path='/admin/home' element={<AdminHomePage />} />
+            <Route path='/admin/home/user/:id' element={<SingleUserPage />} />
+            <Route path='/admin/home/product/:type/:id' element={<SingleProduct />} />
+          </Routes>
+        </div>
+        <MobileNavigation />
+      </Router >
 
 
-  useAuthCheck();
-  useFetchLoggedInUserDetails(loggedInUser);
-
-
-  const routes = createBrowserRouter(
-    createRoutesFromElements(
-      <>
-        <Route path='/signup' element={<SignupPage/>}/>
-        <Route path='/login' element={<LoginPage/>}/>
-        <Route path='/verify-otp' element={<OtpVerificationPage/>}/>
-        <Route path='/forgot-password' element={<ForgotPasswordPage/>}/>
-        <Route path='/reset-password/:userId/:passwordResetToken' element={<ResetPasswordPage/>}/>
-        <Route exact path='/logout' element={<Protected><Logout/></Protected>}/>
-        <Route exact path='/product-details/:id' element={<Protected><ProductDetailsPage/></Protected>}/>
-
-        {
-          loggedInUser?.isAdmin?(
-            // admin routes
-            <>
-            <Route path='/admin/dashboard' element={<Protected><AdminDashboardPage/></Protected>}/>
-            <Route path='/admin/product-update/:id' element={<Protected><ProductUpdatePage/></Protected>}/>
-            <Route path='/admin/add-product' element={<Protected><AddProductPage/></Protected>}/>
-            <Route path='/admin/orders'  element={<Protected><AdminOrdersPage/></Protected>}/>
-            <Route path='*' element={<Navigate to={'/admin/dashboard'}/>}/>
-            </>
-          ):(
-            // user routes
-            <>
-            <Route path='/' element={<Protected><HomePage/></Protected>}/>
-            <Route path='/cart' element={<Protected><CartPage/></Protected>}/>
-            <Route path='/profile' element={<Protected><UserProfilePage/></Protected>}/>
-            <Route path='/checkout' element={<Protected><CheckoutPage/></Protected>}/>
-            <Route path='/order-success/:id' element={<Protected><OrderSuccessPage/></Protected>}/>
-            <Route path='/orders' element={<Protected><UserOrdersPage/></Protected>}/>
-            <Route path='/wishlist' element={<Protected><WishlistPage/></Protected>}/>
-            </>
-          )
-        }
-
-        <Route path='*' element={<NotFoundPage/>} />
-
-      </>
-    )
-  )
-
-  
-  return isAuthChecked ? <RouterProvider router={routes}/> : "";
+    </>
+  );
 }
-
 export default App;
