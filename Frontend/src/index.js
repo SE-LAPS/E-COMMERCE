144d9b68edfe749f1616607b5c71fadaf23e6ef9
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2025-01-18 08:26:06 +0530

    Update index.js

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-import {Provider} from 'react-redux'
-import { store } from './app/store';
-import {ToastContainer} from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
-import { ThemeProvider } from '@mui/material';
-import theme from './theme/theme';
+import Context from './Context/Context';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-        <Provider store={store}>
-              <App />
-              <ToastContainer position='top-right' autoClose={1500} closeOnClick/>
-        </Provider>
-    </ThemeProvider>
+    <Context>
+      <App />
+    </Context>
   </React.StrictMode>
 );
+
+serviceWorkerRegistration.register();
 
