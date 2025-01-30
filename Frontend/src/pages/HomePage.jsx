859269c7c4669365deb0dfc4d7dd70d51d832d20
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2025-01-30 10:37:19 +0530

    Update HomePage.jsx

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,27 +1,77 @@
-import React, { useEffect } from 'react'
-import { Navbar } from '../features/navigation/components/Navbar'
-import { ProductList } from '../features/products/components/ProductList'
-import { resetAddressStatus, selectAddressStatus } from '../features/address/AddressSlice'
-import { useDispatch, useSelector } from 'react-redux'
-import {Footer} from '../features/footer/Footer'
+import React, { useEffect, useState } from 'react';
 
-export const HomePage = () => {
+import { Row, Col } from 'react-bootstrap';
+import { useGetProductsQuery } from '../slices/productsApiSlice';
+import { useSelector } from 'react-redux';
 
-  const dispatch=useDispatch()
-  const addressStatus=useSelector(selectAddressStatus)
+import Product from '../components/Product';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+import Paginate from '../components/Paginate';
+import ProductCarousel from '../components/ProductCarousel';
+import ServerError from '../components/ServerError';
+import Meta from '../components/Meta';
 
-  useEffect(()=>{
-    if(addressStatus==='fulfilled'){
+const HomePage = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPage, setTotalPage] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [limit, setLimit] = useState(0);
+  const [skip, setSkip] = useState(0);
+  const { search } = useSelector(state => state.search);
 
-      dispatch(resetAddressStatus())
+  const { data, isLoading, error } = useGetProductsQuery({
+    limit,
+    skip,
+    search
+  });
+
+  useEffect(() => {
+    if (data) {
+      setLimit(4);
+      setSkip((currentPage - 1) * limit);
+      setTotal(data.total);
+      setTotalPage(Math.ceil(total / limit));
     }
-  },[addressStatus])
+  }, [currentPage, data, limit, total, search]);
+
+  const pageHandler = pageNum => {
+    if (pageNum >= 1 && pageNum <= totalPage && pageNum !== currentPage) {
+      setCurrentPage(pageNum);
+    }
+  };
 
   return (
     <>
-    <Navbar isProductList={true}/>
-    <ProductList/>
-    <Footer/>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>
+          {error?.data?.message || error.error}
+        </Message>
+      ) : (
+        <>
+          {!search && <ProductCarousel />}
+          <Meta />
+          <h1>Latest Products</h1>
+          <Row>
+            {data.products.map(product => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+          {totalPage > 1 && !search && (
+            <Paginate
+              currentPage={currentPage}
+              totalPage={totalPage}
+              pageHandler={pageHandler}
+            />
+          )}
+        </>
+      )}
     </>
-  )
-}
+  );
+};
+
+export default HomePage;
