2a52b54aaed846b0ea46cc8cc2157a2b545e3fed
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2025-01-30 10:38:01 +0530

    Update GenerateToken.js

--- Backend/utils/GenerateToken.js
+++ Backend/utils/GenerateToken.js
@@ -1,6 +1,16 @@
-require('dotenv').config()
-const jwt=require('jsonwebtoken')
+import jwt from 'jsonwebtoken';
 
-exports.generateToken=(payload,passwordReset=false)=>{
-    return jwt.sign(payload,process.env.SECRET_KEY,{expiresIn:passwordReset?process.env.PASSWORD_RESET_TOKEN_EXPIRATION:process.env.LOGIN_TOKEN_EXPIRATION})
-}
+export const generateToken = (req, res, userId) => {
+  // Generating a JWT token for the authenticated user
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: req.body.remember ? 365 * 24 + 'h' : '24h'
+  });
+
+  // Setting the JWT as an HTTP-only cookie for enhanced security
+  res.cookie('jwt', token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== 'development',
+    sameSite: 'strict',
+    maxAge: req.body.remember ? 365 * 24 * 60 * 60 * 1000 : 24 * 60 * 60 * 1000
+  });
+};
