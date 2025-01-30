fa074f68247735f9cef89dac65d927c3bddfe9ec
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2025-01-30 10:37:13 +0530

    Update index.js

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import Context from './Context/Context';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import './assets/styles/bootstrap.custom.css';
+import './assets/styles/index.css';
+import reportWebVitals from './reportWebVitals';
+import Routes from './routes/Routes';
+import store from './store';
+import { Provider } from 'react-redux';
+import { HelmetProvider } from 'react-helmet-async';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <Context>
-      <App />
-    </Context>
+    <HelmetProvider>
+      <Provider store={store}>
+        <Routes />
+      </Provider>
+    </HelmetProvider>
   </React.StrictMode>
 );
 
-serviceWorkerRegistration.register();
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
