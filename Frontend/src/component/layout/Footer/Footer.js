d32ac525a732790b18f35e365ab5fe8c7f7a2781
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2025-02-10 18:24:52 +0530

    Create Footer.js

---
+++ Frontend/src/component/layout/Footer/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer id="footer">
+      
+      <div className="midFooter">
+        <h1>ECOMMERCE</h1>
+        {/* <img src={logo} alt="logo" /> */}
+
+        <p>High Quality is our first priority</p>
+        <p>Copyrights 2023 &copy; MumukshTayal</p>
+      </div>
+      
+    </footer>
+  );
+};
+
+export default Footer;
